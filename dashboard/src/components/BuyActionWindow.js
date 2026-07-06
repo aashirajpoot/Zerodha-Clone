d3c277d78e7ebac302f04be0de3d464a00518969
Aashi Rajpoot <aashirajpoot2004@gmail.com>
2026-07-06 12:09:02 +0530

    Changes made to dashboard after deployment of backend

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3001/newOrder", {
+    axios.post("https://zerodha-clone-qwrf.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
